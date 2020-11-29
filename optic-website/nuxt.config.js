218812d94481a152f62baa9cdcdcc2d2d38c2702
Samuel Palmer <palmer_s@ymail.com>
2020-11-29 17:39:01 +1100

    js error
    js error

--- optic-website/nuxt.config.js
+++ optic-website/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap" rel="stylesheet">'}
+      { rel: 'stylesheet', href: '"https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap"'}
     ]
   },
 
